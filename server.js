e0fb12399bc34abcc7b19fb26cc56ecbe847516c
hoffman1200 <hoffman1200@hotmail.com>
2020-07-18 10:35:19 -0500

    API places working

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 require("./routes/post-api-routes.js")(app);
 
+
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
     app.listen(PORT, () => {
